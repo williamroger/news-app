3f6fbc31e258037832203bc8d135abab077394f3
William Roger <williamroger.frontend@gmail.com>
2019-08-16 08:40:01 -0300

    Adicionado Segment para categorias

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
+  @ViewChild(IonSegment) segment: IonSegment;
+  categorias = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
   constructor() {}
 
+  ngOnInit() {
+    this.segment.value = this.categorias[0];
+  }
 }
