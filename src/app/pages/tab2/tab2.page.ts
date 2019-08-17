0b2f82100e5168e5107214076f5847626171cf98
William Roger <williamroger.frontend@gmail.com>
2019-08-17 11:27:28 -0300

    Alterando as categorias de notícias

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NoticiasService } from 'src/app/services/noticias.service';
+import { Artigo } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -12,14 +13,25 @@ export class Tab2Page implements OnInit {
   @ViewChild(IonSegment, {static: true}) segment: IonSegment;
 
   categorias = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  noticias: Artigo[] = [];
 
   constructor(private noticiasService: NoticiasService) {}
 
   ngOnInit() {
     this.segment.value = this.categorias[0];
-    this.noticiasService.getTopHeadlineCategoria(this.categorias[0])
-        .subscribe(resp => {
-          console.log(resp);
-        });
+    this.carregarNoticias(this.categorias[0]);
+  }
+
+  alterarCategoria(event) {
+    this.noticias = [];
+    this.carregarNoticias(event.detail.value);
+  }
+
+  carregarNoticias(categoria: string) {
+    this.noticiasService.getTopHeadlineCategoria(categoria)
+      .subscribe(resp => {
+        console.log(resp);
+        this.noticias.push(...resp.articles);
+      });
   }
 }
